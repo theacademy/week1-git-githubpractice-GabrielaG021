c1795f53d8a4aed83687f74b96490975fe481b81
Gabriela Gutierrez <gabystephane200399@gmail.com>
2023-05-09 13:59:43 -0400

    Added new NetBeans Hello World & Shape

---
+++ Day 2/CodeAlongNetBeansHelloWorld/HelloWorld/src/main/java/com/gabrielag/helloworld/HelloWorld.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gabrielag.helloworld;
+
+/**
+ *@author Gabriela Gutierrez
+ * email: gabystephane@hotmail.com
+ * date: May 9th, 2023
+ * Purpose: My first Hello World program in an IDE
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //Class //Object //Operator
+        
+        System.out.println("  *  \n * * \n*   *\n * * \n  *  \n");
+        
+        System.out.println("  *  ");
+        System.out.println(" * * ");
+        System.out.println("*   *");
+        System.out.println(" * * ");
+        System.out.println("  *  ");
+        
+        
+    }
+}
